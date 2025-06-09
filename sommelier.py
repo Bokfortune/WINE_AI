993d5cd28de12296a051e2389d56abb16182acf7
Bokfortune <jibok815@naver.com>
2025-06-09 15:42:12 +0900

    'modify'

--- sommelier.py
+++ sommelier.py
@@ -8,7 +8,3 @@ from langchain_pinecone import PineconeVectorStore
 ########################################################################
 load_dotenv()
 
-OPENAAI_LLM_MODEL = 'gpt-4o-mini'
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "us-west1-gcp")
-PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "wine-reviews")
